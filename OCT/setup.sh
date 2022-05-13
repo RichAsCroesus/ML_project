e37c4925c1e8ea66b90f8e40b636c47be0cf1177
Rich Malloy <rmalloy@nasuni.com>
2022-05-13 06:07:07 -0400

    stuff

---
+++ OCT/setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+# bootstrap models getbootstrap.com 
+# flask bootstap extension... don't need it if you know bootstrap
+
+export FLASK_APP=process_model.py
+export FLASK_DEBUG=1
--- models/OCT/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-export FLASK_APP=process_model.py
-export FLASK_DEBUG=1
