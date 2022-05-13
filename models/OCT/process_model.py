a465d6ca6b5bef7291090330ae7d91f2536fe27a
Rich Malloy <rmalloy@nasuni.com>
2022-05-13 05:24:31 -0400

    first pass

---
+++ models/OCT/process_model.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+import tensorflow as tf
+
+
+# ecport FLASK_APP=process_model.py
+app = Flask(__name__, template_folder='./templates', static_folder='./static')
+
+@app.route("/")
+@app.route("/home")
+def main_page():
+    return render_template('home.html') # ,posts=posts
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/process')
+def process(image):
+    return render_template('process.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models/OCT/setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+export FLASK_APP=process_model.py
+export FLASK_DEBUG=1
